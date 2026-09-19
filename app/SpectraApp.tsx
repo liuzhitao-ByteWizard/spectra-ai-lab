f34803ed57b8ded5651edb390b1b3255cb138479
Xiaohai0103 <songxiaohai603@gmail.com>
2026-09-19 13:06:25 +0800

    fix: tighten homepage yellow doublet

--- app/SpectraApp.tsx
+++ app/SpectraApp.tsx
@@ -400,7 +400,7 @@ const AnalysisGlassPanel = memo(function AnalysisGlassPanel() {
           <i
             key={line.wavelengthNm}
             className={`${index <= stage || (index === 4 && stage >= 3) ? "visible" : ""}${index === 3 ? " yellow-doublet-left" : ""}${index === 4 ? " yellow-doublet-right" : ""}`}
-            style={{ left: `${[13, 28, 57, 75, 81][index]}%`, backgroundColor: line.color }}
+            style={{ left: `${[13, 28, 57, 75.4, 77.2][index]}%`, backgroundColor: line.color }}
           >
             <b>{line.wavelengthNm.toFixed(2)}</b>
           </i>
